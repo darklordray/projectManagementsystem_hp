7b2979b848b6730e314c35a53511cd6d73133c25
darklordray <rddesign0@gmail.com>
2021-04-16 11:54:45 +0200

    Added menu selections the required task, outstanding is the Finialize Project for displaying necessary data

--- projectManagementSystem.java
+++ projectManagementSystem.java
@@ -2,25 +2,72 @@ import java.util.Scanner;
 
 public class projectManagementSystem {
     public static void main(String[] args) {
+        
+        //Display options to select for input and changes
 
         Scanner numberInput = new Scanner(System.in);
         while (true) {
-            System.out.println("Please select an option from the menu below");
-            System.out.println("1:\tChange the due date of a project");
-            System.out.println("2:\tPay into a project");
-            System.out.println("3:\tUpdate a project manager details");
-            System.out.println("4:\tFinalise a project");
-            System.out.println("5:\tDisplay outstanding project details");
-            System.out.println("6:\tDisplay overdue project details");
-            System.out.println("7:\tCreate new project");
-            System.out.println("0:\tSave and Quit\n");
+            System.out.println("What would you like to do?");
+            System.out.println("1:\tChange the due date of a project:");
+            System.out.println("2:\tUpdate payment information:");
+            System.out.println("3:\tUpdate project manager details:");
+            System.out.println("4:\tFinalise a project:");
+            System.out.println("5:\tDisplay outstanding project details:");
+            System.out.println("6:\tDisplay overdue project details:");
+            System.out.println("7:\tCreate new project:");
+            System.out.println("8:\tSave and Quit\n");
             int choice = numberInput.nextInt();
+            
+            //Scanner inputs for menu selection and funtion
 
-            // Close the scanner and quit the program
-            if (choice == 0) {
+            if (choice == 8) {
                 System.out.println("Shutting down");
                 break;
             }
+
+            if (choice == 1) {
+                Scanner projectNumberScanner = new Scanner(System.in);
+                System.out.println("Enter number of the project that you want to edit: ");
+                String project_number = projectNumberScanner.nextLine();
+                
+                Scanner deadlineScanner = new Scanner(System.in);
+                System.out.println("Enter new due date of the project:");
+                String deadline = deadlineScanner.nextLine();
+            }
+
+            if (choice == 2){
+                Scanner projectNumberScanner = new Scanner(System.in);
+                System.out.println("Enter number of the project that you want to update");
+                String project_number = projectNumberScanner.nextLine();
+
+                Scanner cost_owedScanner = new Scanner(System.in);
+                System.out.println("Enter updated cost owing to the project: ");
+                Integer cost_owed = cost_owedScanner.nextLine();
+            }
+
+            if (choice == 3) {
+                Scanner titleScanner = new Scanner(System.in);
+                System.out.println("Enter titles of the contractor: ");
+                String title = titleScanner.nextLine();
+                
+                Scanner nameScanner = new Scanner(System.in);
+                System.out.println("Enter the name of the contractor:");
+                String name = nameScanner.nextLine();
+
+                Scanner telephone_numberScanner = new Scanner(System.in);
+                System.out.println("Enter the telephone number of the contractor:");
+                Interger telephone_number = telephone_numberScanner.nextLine();
+
+                Scanner email_addressScanner = new Scanner(System.in);
+                System.out.println("Enter the email address of the contractor:");
+                String email_address = email_addressScanner.nextLine();
+
+                Scanner addressScanner = new Scanner(System.in);
+                System.out.println("Enter the address of the contractor:");
+                String address = addressScanner.nextLine();
+
+            }
+
             if (choice == 7) {
                 Scanner projectNumberScanner = new Scanner(System.in);
                 System.out.println("Enter number of the project");
@@ -54,6 +101,7 @@ public class projectManagementSystem {
                 System.out.println("Enter the dead line of the project");
                 String deadline = deadlineScanner.nextLine();
             }
+        
         }
     }
 }
